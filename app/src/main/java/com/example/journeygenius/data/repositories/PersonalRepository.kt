60761ebfd149c76487f7922d45a38f076fc7f8e4
Weiru He <lingqingran@gmail.com>
2023-04-16 20:33:13 -0400

    edit personal repository

--- app/src/main/java/com/example/journeygenius/data/repositories/PersonalRepository.kt
+++ app/src/main/java/com/example/journeygenius/data/repositories/PersonalRepository.kt
@@ -2,11 +2,36 @@ package com.example.journeygenius.data.repositories
 
 import com.example.journeygenius.data.PersonalDao
 import com.example.journeygenius.data.models.Personal
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
-
+@ViewModelScoped
 class PersonalRepository @Inject constructor(
-    private val persinalDao: PersonalDao
+    private val personalDao: PersonalDao
 ){
-    val getAllTasks: Flow<List<Personal>> = persinalDao.getAllProfile()
+    val getAllProfile: Flow<List<Personal>> = personalDao.getAllProfile()
+
+    fun getSelectedProfile(id: Int): Flow<Personal> {
+        return personalDao.getSelectedProfile(id)
+    }
+
+    suspend fun addProfile(personal: Personal) {
+        personalDao.addProfile(personal)
+    }
+
+    suspend fun updateProfile(personal: Personal) {
+        personalDao.updateProfile(personal)
+    }
+
+    suspend fun deleteProfile(personal: Personal) {
+        personalDao.deleteProfile(personal)
+    }
+
+    suspend fun deleteAllProfiles() {
+        personalDao.deleteAllProfiles()
+    }
+
+    fun searchProfile(searchQuery: String): Flow<List<Personal>> {
+        return personalDao.searchProfile(searchQuery = searchQuery)
+    }
 }
--- app/src/main/java/com/example/journeygenius/personal/PersonalViewModel.kt
+++ app/src/main/java/com/example/journeygenius/personal/PersonalViewModel.kt
@@ -4,7 +4,30 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.journeygenius.data.models.Personal
+import com.example.journeygenius.data.repositories.PersonalRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+//@HiltViewModel
+//class PersonalViewModel @Inject constructor(
+//    private val personalRepository: PersonalRepository
+//) : ViewModel() {
+//    private val _allProfiles = MutableStateFlow<List<Personal>>(emptyList())
+//    val allProfiles: StateFlow<List<Personal>> = _allProfiles
+//
+//    fun getAllProfiles() {
+//        viewModelScope.launch {
+//            personalRepository.getAllProfile.collect {
+//                _allProfiles.value = it
+//            }
+//        }
+//    }
+//}
 class PersonalViewModel : ViewModel() {
     private var _email = mutableStateOf(TextFieldValue())
     val email: MutableState<TextFieldValue> = _email
