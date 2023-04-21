58e705ad4d0d1c16f3d9d82d74749edac4fcdd9d
Zeyu Su <suz15@miamioh.edu>
2023-04-21 19:29:36 -0400

    connect personal screen with real user data

--- app/src/main/java/com/example/journeygenius/LoginScreen.kt
+++ app/src/main/java/com/example/journeygenius/LoginScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
