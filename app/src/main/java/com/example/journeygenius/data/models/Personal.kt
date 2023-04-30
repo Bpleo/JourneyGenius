0fabaf10761b0533bda14decd587b065e17593c0
Zeyu Su <suz15@miamioh.edu>
2023-04-30 17:38:54 -0400

    update personal data class

--- app/src/main/java/com/example/journeygenius/data/models/Personal.kt
+++ app/src/main/java/com/example/journeygenius/data/models/Personal.kt
@@ -4,11 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.journeygenius.ui.util.Constants.PERSONAL_TABLE
 
-@Entity(tableName = PERSONAL_TABLE)
 data class Personal (
-    @PrimaryKey
     val id: String,
     val userName: String,
     val email: String,
-    val pwd: String
-    )
+    val pwd: String)
