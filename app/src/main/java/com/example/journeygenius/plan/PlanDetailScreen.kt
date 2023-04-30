034062c89ee87245b8aa0d135c28a69145a0cfbc
Weiru He <lingqingran@gmail.com>
2023-04-30 15:51:01 -0400

    add scroll bar to details

--- app/src/main/java/com/example/journeygenius/plan/PlanDetailScreen.kt
+++ app/src/main/java/com/example/journeygenius/plan/PlanDetailScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -32,80 +34,87 @@ fun PlanDetail(navController: NavController, viewModel: JourneyGeniusViewModel){
                 Text(text = "Summary",
                     fontSize = MaterialTheme.typography.headlineLarge.fontSize)
             }
-            Column(
+            Box(
                 modifier = Modifier
-                    .padding(25.dp, 70.dp)
                     .fillMaxWidth()
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-            ){
-                Column(modifier = Modifier.padding(15.dp,20.dp)) {
-                    Text(text = "Date: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(text =planOnDetail.value.date,
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize
-                    )
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Text(text = "Destination: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize)
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(text =planOnDetail.value.destination,
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize
-                    )
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Text(text = "Attractions: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize)
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Box(modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(25.dp, 70.dp)
                         .fillMaxWidth()
-                        .height(90.dp)){
-                        LazyColumn(modifier = Modifier.fillMaxWidth())
-                        {
-                            items(planOnDetail.value.attractions,key={it.name}){
-                                Text(text = it.name,
-                                    fontSize = MaterialTheme.typography.titleLarge.fontSize
-                                )
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                ){
+                    Column(modifier = Modifier.padding(15.dp,20.dp)) {
+                        Text(text = "Date: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text =planOnDetail.value.date,
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                        )
+                        Spacer(modifier = Modifier.height(25.dp))
+                        Text(text = "Destination: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text =planOnDetail.value.destination,
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                        )
+                        Spacer(modifier = Modifier.height(25.dp))
+                        Text(text = "Attractions: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Box(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(90.dp)){
+                            LazyColumn(modifier = Modifier.fillMaxWidth())
+                            {
+                                items(planOnDetail.value.attractions,key={it.name}){
+                                    Text(text = it.name,
+                                        fontSize = MaterialTheme.typography.titleLarge.fontSize
+                                    )
+                                }
                             }
                         }
+
+                        Spacer(modifier = Modifier.height(15.dp))
+                        Text(text = "Price Level: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "${planOnDetail.value.priceLevel}: ${planOnDetail.value.priceLevelLabel}",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                        )
+                        Spacer(modifier = Modifier.height(35.dp))
+                        Text(text = "Hotel: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        // TODO:
+                        Box(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(90.dp)){
+                            LazyColumn(modifier = Modifier.fillMaxWidth())
+                            {
+                                items(planOnDetail.value.hotel,key={it.place.name}){
+                                    Text(text = it.place.name,
+                                        fontSize = MaterialTheme.typography.titleLarge.fontSize
+                                    )
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(15.dp))
+                        Text(text = "Transport Mode: ",
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = planOnDetail.value.travelType,
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize
+                        )
+                        Spacer(modifier = Modifier.height(75.dp))
                     }
 
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Text(text = "Price Level: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize)
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(text = "${planOnDetail.value.priceLevel}: ${planOnDetail.value.priceLevelLabel}",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize
-                    )
-                    Spacer(modifier = Modifier.height(35.dp))
-                    Text(text = "Hotel: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize)
-                    Spacer(modifier = Modifier.height(10.dp))
-                    // TODO:  
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .height(90.dp)){
-                        LazyColumn(modifier = Modifier.fillMaxWidth())
-                        {
-                            items(planOnDetail.value.hotel,key={it.place.name}){
-                                Text(text = it.place.name,
-                                    fontSize = MaterialTheme.typography.titleLarge.fontSize
-                                )
-                            }
-                        }
-                    }
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Text(text = "Transport Mode: ",
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize)
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(text = planOnDetail.value.travelType,
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize
-                    )
-                    Spacer(modifier = Modifier.height(75.dp))
+
                 }
-
-
             }
+
             Box(modifier = Modifier
                 .fillMaxSize()
                 .padding(bottom = 80.dp),
