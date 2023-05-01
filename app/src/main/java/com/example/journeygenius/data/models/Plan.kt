90b480a48d6b072b30fe3bb2ca86e6dc132293f5
Weiru He <lingqingran@gmail.com>
2023-04-30 22:47:30 -0400

    annotate old plan

--- app/src/main/java/com/example/journeygenius/data/models/Plan.kt
+++ app/src/main/java/com/example/journeygenius/data/models/Plan.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.journeygenius.ui.util.Constants.PLAN_TABLE
 
+/* Attention: To be Deleted */
 @Entity(tableName = PLAN_TABLE)
 data class Plan(
     val title: String,
