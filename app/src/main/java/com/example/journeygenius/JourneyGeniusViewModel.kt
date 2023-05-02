4013f939f4718902752bb0e75c6bedae502dd9f0
Zeyu Su <suz15@miamioh.edu>
2023-05-01 22:56:39 -0400

    fix photo problem

--- app/src/main/java/com/example/journeygenius/JourneyGeniusViewModel.kt
+++ app/src/main/java/com/example/journeygenius/JourneyGeniusViewModel.kt
@@ -66,7 +66,7 @@ class JourneyGeniusViewModel(
         updateVerifyPwd("")
     }
 
-    //TODO pull plan list from firestore and add to local vm
+    //pull plan list from firestore and add to local vm
     fun signIn() {
         val user = auth.currentUser
         if (user != null) {
@@ -706,7 +706,7 @@ class JourneyGeniusViewModel(
                     location = Location(result.geometry.location.lat, result.geometry.location.lng),
                     rating = result.rating,
                     place_id = result.place_id,
-                    photos = result.photos.toList()
+                    photos = result.photos?.toList()
                 )
             })
             Log.d("attractionlist", attractionsList.toString())
@@ -731,7 +731,7 @@ class JourneyGeniusViewModel(
                     location = Location(result.geometry.location.lat, result.geometry.location.lng),
                     rating = result.rating,
                     place_id = result.place_id,
-                    photos = result.photos.toList()
+                    photos = result.photos?.toList()
                 )
             }
         }
@@ -792,7 +792,7 @@ class JourneyGeniusViewModel(
                         ),
                         rating = result.rating,
                         place_id = result.place_id,
-                        photos = result.photos.toList()
+                        photos = result.photos?.toList()
                     ),
                     priceLevel = result.price_level,
 
@@ -810,7 +810,7 @@ class JourneyGeniusViewModel(
                         ),
                         rating = result.rating,
                         place_id = result.place_id,
-                        photos = result.photos.toList()
+                        photos = result.photos?.toList()
                     ),
                     priceLevel = result.price_level,
 
