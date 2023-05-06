672c5c138f9cfdc8075f8dc1861e0b4aeb0a9346
Zeyu Su <suz15@miamioh.edu>
2023-05-06 16:38:30 -0400

    clean up code

--- app/src/main/java/com/example/journeygenius/App.kt
+++
@@ -1,7 +0,0 @@
-package com.example.journeygenius
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()
--- app/src/main/java/com/example/journeygenius/data/models/TravelType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.journeygenius.data.models
-
-enum class TravelType {
-    SINGLE,
-    FAMILY,
-    BUSINESS
-}
